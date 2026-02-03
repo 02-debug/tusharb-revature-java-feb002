8cf31d8561376528621ee3dbd51042dc6eb68e25
02-debug <bhuretushar7@gmail.com>
2026-02-03 21:53:37 +0530

    Fifth Project

---
+++ week0102_javabasics/src/week0102_javabasics/program5.java
@@ -0,0 +1,11 @@
+package week0102_javabasics;
+
+public class program5 {
+	public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        System.out.println("Result = " + (x + y));
+    }
+}
+
+
