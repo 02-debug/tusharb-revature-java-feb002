ccbd4aec8c578f06a2ecf2a33d556b0a7302a739
02-debug <bhuretushar7@gmail.com>
2026-02-03 21:47:13 +0530

    Third Project

---
+++ week0102_javabasics/src/week0102_javabasics/program3.java
@@ -0,0 +1,12 @@
+package week0102_javabasics;
+
+public class program3 {
+	    
+	    	public static void main(String[] args) {
+	            int a = 5;
+	            int b = 3;
+	            int sum = a + b;
+	            System.out.println("Sum = " + sum);
+	        }
+	    }
+
