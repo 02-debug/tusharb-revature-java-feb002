889c6a919192df8a18478da3b5b1bee569ddacb3
02-debug <bhuretushar7@gmail.com>
2026-02-03 21:50:45 +0530

    Fourth Project

---
+++ week0102_javabasics/src/week0102_javabasics/program4.java
@@ -0,0 +1,15 @@
+package week0102_javabasics;
+
+public class program4 {
+	   public static void main(String[] args) {
+	        int num = 7;
+
+	        if (num % 2 == 0) {
+	            System.out.println("Even");
+	        } else {
+	            System.out.println("Odd");
+	        }
+	    }
+	}
+
+
