99a4103f40a49a22d0c12fe3a0a0800d232efe71
02-debug <bhuretushar7@gmail.com>
2026-02-03 21:56:16 +0530

    Sixth Project

---
+++ week0102_javabasics/src/week0102_javabasics/program6.java
@@ -0,0 +1,17 @@
+package week0102_javabasics;
+
+public class program6 {
+	 public static void main(String[] args) {
+	        int num = 1234;
+	        int reverse = 0;
+
+	        while (num != 0) {
+	            reverse = reverse * 10 + num % 10;
+	            num = num / 10;
+	        }
+
+	        System.out.println("Reversed number: " + reverse);
+	    }
+	}
+
+
