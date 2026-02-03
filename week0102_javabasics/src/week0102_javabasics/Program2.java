5c285b2995c1c1133e4389a7b428e6a6b2cf929c
02-debug <bhuretushar7@gmail.com>
2026-02-03 21:36:09 +0530

    second  project

--- week0102_javabasics/src/week0102_javabasics/Program2.java
+++ week0102_javabasics/src/week0102_javabasics/Program2.java
@@ -1,10 +1,9 @@
 package week0102_javabasics;
 
-public class Program1 {
+public class Program2 {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		
-		
-	
-	}
+		System.out.println(1429+1247083);
+}
 }
